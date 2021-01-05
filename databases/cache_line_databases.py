01b673b491478b29d5815e56683a3574b41c0b04
Erwan <erwan.pannier@centralesupelec.fr>
2021-01-05 02:50:30 +0100

    cache only one path at a time to prevent Memory errors

--- databases/cache_line_databases.py
+++ databases/cache_line_databases.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 """ Load all all pre-configured databases in ~/.radis to generate cache files """
 
-from radis import SpectrumFactory
-from radis.misc.config import getDatabankList
+from radis.misc.utils import get_files_from_regex
+from radis.misc.config import getDatabankList, getDatabankEntries
 
-sf = SpectrumFactory(wavenum_min = 100, 
-                     wavenum_max = 20000,
-                    isotope='1,2,3')
-for db in getDatabankList():
-    sf.input.molecule = None
-    sf.load_databank(db)
-# Note: for CO2 we're loading the whole database. May need > 2 GB RAM. 
+from radis import SpectrumFactory
+
+sf = SpectrumFactory(1000, 10000,
+                     isotope='1,2,3')
+
+for dbname in getDatabankList():
+    print(dbname)
+
+    db0 = getDatabankEntries(dbname)
+
+    # Read only one path at a time to generate the cache file. 
+    # (prevents Memory errors)
+    for path in db0['path']:
+        path = get_files_from_regex(path)
+        print('...', path)
+        db = {k:db0[k] for k in db0.keys() if k not in ['info', 'levelszpe']}
+        db['path'] = path
+        
+        sf.load_databank(**db)
