07ac7f5683b64e66538ca3603737b9e76904a4fc
Erwan <erwan.pannier@centralesupelec.fr>
2021-01-06 10:55:47 +0100

    simplify environment (Qt not needed directly) fix typo in HITEMP CO wget

--- databases/cache_line_databases.py
+++ databases/cache_line_databases.py
@@ -11,7 +11,7 @@ sf = SpectrumFactory(wavenum_min=50,
                      isotope='1,2,3')
 
 for dbname in getDatabankList():
-    print(dbname)
+    print("Caching", dbname)
 
     db0 = getDatabankEntries(dbname)
 
