e7ec96f72d08f54964dd769146de558c2644a8c5
Erwan Pannier <erwan.pannier@gmail.com>
2021-06-24 19:31:08 +0200

    preconfigure H2O files preconfigure CO using the new fetch_databank('hitemp')

--- databases/cache_line_databases.py
+++ databases/cache_line_databases.py
@@ -8,20 +8,30 @@ from radis import SpectrumFactory
 
 sf = SpectrumFactory(wavenum_min=50,
                      wavenum_max=10000,
-                     isotope='1,2,3')
+                     isotope='1,2,3',
+                     wstep=10   # no calculation anyway
+                     )
 
+# Cache H2O and CO2 downloaded in postBuild
 for dbname in getDatabankList():
     print("Caching", dbname)
 
     db0 = getDatabankEntries(dbname)
 
-    # Read only one path at a time to generate the cache file. 
+    # Read only one path at a time to generate the cache file.
     # (prevents Memory errors)
     for path in db0['path']:
         path = get_files_from_regex(path)
         print('...', path)
         db = {k:db0[k] for k in db0.keys() if k not in ['info', 'levelszpe']}
         db['path'] = path
-        
+
         sf.input.molecule=""
         sf.load_databank(**db)
+
+
+# Download other species
+for molecule in ['CO']:
+    print("Downloading and caching ", molecule)
+    sf.input.molecule = molecule
+    sf.fetch_databank('hitemp')
